434a5183b1619b174c6dc3a6d68619a4d58d9d33
Enmanuel De los Santos Cruz <116897148+EnmaSantos@users.noreply.github.com>
2024-09-23 13:44:38 -0600

    commit

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,10 @@
-print("Hello, World!")
+print("Hello, World!") 
+    # The  print()  function is used to display the message "Hello, World!" on the screen.
+
+# Run the script
+# To run the script, you can use the following command:
+# python hello_world.py
+# Output
+# Hello, World! 
+    # The output of the script will be displayed on the screen.
+    
